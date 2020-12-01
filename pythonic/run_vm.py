11a912fca60626fc598a6d1677730982d2d1c8cf
Sagnik Bhattacharya <sagnikdps@gmail.com>
2020-11-30 20:28:15 -0800

    memory implemented, no reg file anymore

--- pythonic/run_vm.py
+++ pythonic/run_vm.py
@@ -27,7 +27,8 @@ args = parser.parse_args()
 
 code: List[Tuple[str, Optional[int]]]
 with open(args.file, "r") as f:
-    code = list(filter(lambda x: x is not None,
-                       map(parse_line, f.readlines())))  # type: ignore
+    code = list(
+        filter(lambda x: x is not None, map(parse_line, f.readlines()))
+    )  # type: ignore
 
-execute(code)
+execute(code, stack=[], mem=[0 for _ in range(0xFFFF)])
--- pythonic/virtual_machine_pt1.py
+++ pythonic/virtual_machine_pt1.py
@@ -3,80 +3,102 @@ from dataclasses import dataclass
 from typing import List, Optional, Tuple, Union
 
 
-def execute(
-    code: List[Tuple[str, Optional[int]]], stack: List[int], mem: List[int]
-) -> List[int]:
-    def getIp() -> int:
-        return mem[0]
+@dataclass
+class RegisterFile:
+    ip: int = 0
+    sp: int = 0
+    x0: int = 0
+    x1: int = 0
+    x2: int = 0
+    x3: int = 0
+    x4: int = 0
+    x5: int = 0
+    x6: int = 0
+    x7: int = 0
 
-    def setIp(newIp: int) -> None:
-        mem[0] = newIp
-
-    def incIp() -> None:
-        mem[0] += 1
-
-    def to_int(maybeAddr: Optional[int], msg: str) -> int:
-        if maybeAddr is None:
-            raise ValueError(msg)
+    def __getitem__(self, idx: int) -> int:
+        if idx == 0:
+            return self.ip
+        elif idx == 1:
+            return self.sp
         else:
-            return maybeAddr
+            return self.__dict__[f"x{idx - 2}"]
+
+    def __setitem__(self, idx: int, val: int) -> None:
+        if idx == 0:
+            self.ip = val
+        elif idx == 1:
+            self.sp = val
+        else:
+            # eval(f"self.x{idx - 2} = {val}")
+            self.__dict__[f"x{idx - 2}"] = val
+
+
+def execute(code: List[Tuple[str, Optional[int]]]) -> List[int]:
+    stack: List[int] = []
+    registerFile = RegisterFile()
 
     while True:
-        instruction = code[getIp()][0]
+        instruction = code[registerFile.ip][0]
         if instruction == "halt":
             # halt program execution
             break
 
         elif instruction == "jmp":
             # jump to the address at the top of the stack
-            setIp(stack.pop())
+            registerFile.ip = stack.pop()
 
         elif instruction == "push":
             # push an integer to the stack
             number = to_int(
-                code[getIp()][1], "`push` instruction requires an integer argument."
+                code[registerFile.ip][1],
+                "`push` instruction requires an integer argument.",
             )
             stack.append(number)
-            incIp()
+            registerFile.ip += 1
 
         elif instruction == "load":
             addr = to_int(
-                code[getIp()][1], "`load` instruction requires an address to load from."
+                code[registerFile.ip][1],
+                "`load` instruction requires an address to load from.",
             )
-            if addr >= 0:
-                stack.append(mem[addr])
-            else:
-                raise ValueError(f"Invalid address {addr}")
-
-            incIp()
-
-        elif instruction == "store":
-            addr = to_int(
-                code[getIp()][1], "`store` instruction requires an address to store to."
-            )
-            if 0 <= addr < 10:  # store to register file
-                mem[addr] = stack.pop()
+            if 0 <= addr < 10:  # load from register file
+                stack.append(registerFile[addr])
             elif addr > 10:
                 raise NotImplementedError("Need to implement RAM.")
             else:
                 raise ValueError(f"Invalid address {addr}")
 
-            incIp()
+            registerFile.ip += 1
+
+        elif instruction == "store":
+            addr = to_int(
+                code[registerFile.ip][1],
+                "`store` instruction requires an address to store to.",
+            )
+            if 0 <= addr < 10:  # store to register file
+                registerFile[addr] = stack.pop()
+            elif addr > 10:
+                raise NotImplementedError("Need to implement RAM.")
+            else:
+                raise ValueError(f"Invalid address {addr}")
+
+            registerFile.ip += 1
 
         elif instruction == "add":
             # add the top two ints on the stack (and pop them), push the result
             stack.append(stack.pop() + stack.pop())
-            incIp()
+            registerFile.ip += 1
 
         elif instruction == "sub":
             # sub the top two ints on the stack (and pop them), push the result
             stack.append(stack.pop() - stack.pop())
-            incIp()
+            registerFile.ip += 1
 
         elif instruction == "mul":
             # sub the top two ints on the stack (and pop them), push the result
             stack.append(stack.pop() * stack.pop())
-            incIp()
+            registerFile.ip += 1
 
         elif instruction == "cmp":
             # pop the top and second, push sign(top - second)
@@ -89,45 +111,54 @@ def execute(
                     return -1
 
             stack.append(comparator(stack.pop(), stack.pop()))
-            incIp()
+            registerFile.ip += 1
 
         elif instruction == "beq":
             # if the result of the previous comparison was 0 then branch
             cmp_result = stack.pop()
             addr = to_int(
-                code[getIp()][1], "`beq` instruction requires an address to branch to."
+                code[registerFile.ip][1],
+                "`beq` instruction requires an address to branch to.",
             )
             if cmp_result == 0:
-                setIp(addr)
+                registerFile.ip = addr
             else:
-                incIp()
+                registerFile.ip += 1
 
         elif instruction == "bgt":
             # if the result of the previous comparison was 1 then branch
             cmp_result = stack.pop()
             addr = to_int(
-                code[getIp()][1], "`bgt` instruction requires an address to branch to."
+                code[registerFile.ip][1],
+                "`bgt` instruction requires an address to branch to.",
             )
             if cmp_result == 1:
-                setIp(addr)
+                registerFile.ip = addr
             else:
-                incIp()
+                registerFile.ip += 1
 
         elif instruction == "print":
             # just print the top of the stack
             print(stack[-1])
-            incIp()
+            registerFile.ip += 1
 
         elif instruction == "input":
             # read an integer from stdin and push it to the top of the stack
             stack.append(int(input(">>> ")))
-            incIp()
+            registerFile.ip += 1
 
         elif instruction == "noop":
             # don't do anything (apparently this is useful?)
-            incIp()
+            registerFile.ip += 1
 
         else:
-            raise ValueError(f"Unknown instruction {code[getIp()]}")
+            raise ValueError(f"Unknown instruction {code[registerFile.ip]}")
 
     return stack
+
+
+def to_int(maybeAddr: Optional[int], msg: str) -> int:
+    if maybeAddr is None:
+        raise ValueError(msg)
+    else:
+        return maybeAddr
